24fab454ad04032027aed44059ab6e2598c661f5
awkperl <infinixhp9548@gmail.com>
2026-06-22 14:44:55 +0300

    analytics dashboard fix

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,4 +1,4 @@
-/**const express = require("express");
+const express = require("express");
 const router = express.Router();
 const upload = require("../config/multer");
 const auth = require("../middleware/auth");
@@ -8,42 +8,6 @@ router.post("/", auth, upload.single("file"), (req, res) => {
     //file_url: `http://localhost:5000/uploads/${req.file.filename}`
      file_url: req.file.path
   });
-});
-
-module.exports = router;**/
-const express = require("express");
-const router = express.Router();
-const upload = require("../config/multer");
-const auth = require("../middleware/auth");
-
-router.post("/", auth, (req, res) => {
-
-  upload.single("file")(req, res, (err) => {
-
-    if (err) {
-      console.error("UPLOAD ERROR:");
-      console.error(err);
-      console.error(err.message);
-
-      return res.status(500).json({
-        error: err.message
-      });
-    }
-
-    console.log("FILE RECEIVED:", req.file);
-
-    if (!req.file) {
-      return res.status(400).json({
-        error: "No file uploaded"
-      });
-    }
-
-    res.json({
-      file_url: req.file.path
-    });
-
-  });
-
 });
 
 module.exports = router;
